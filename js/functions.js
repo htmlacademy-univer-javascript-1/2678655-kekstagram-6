2179aa57a2d9151380079ad71748dced17cea6b1
Egor Osipchuk <kaban.kabanytch@yandex.ru>
2025-11-20 19:34:11 +0700

    редактирование под eslint проверку

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,26 @@
 function isLengthValid(str, len) {
   return str.length <= len;
 }
+console.log(isLengthValid('test', 5));// eslint-disable-line no-console
 
-function isPalindrome(str){
-  const validateStroke = str.replaceAll(' ','').toLowerCase();
+function isPalindrome(str) {
+  const validateStroke = str.replaceAll(' ', '').toLowerCase();
   const reversedStroke = validateStroke.split('').reverse().join('');
   return validateStroke === reversedStroke;
 }
+console.log(isPalindrome('А роза упала на лапу Азора'));// eslint-disable-line no-console
 
 function number(str) {
   let result = '';
-  const validateStroke = str.replaceAll(' ','');
-  if(validateStroke)
-  for (let i = 0; i < validateStroke.length; i++) {
-    if (!isNaN(validateStroke[i])) {
-      result += validateStroke[i];
+  const validateStroke = str.replaceAll(' ', '');
+  if (validateStroke) {
+    for (let i = 0; i < validateStroke.length; i++) {
+      if (!isNaN(validateStroke[i])) {
+        result += validateStroke[i];
+      }
     }
   }
   return result;
 }
-console.log(number('а я томат'))
+
+console.log(number('а я томат и жизни рад'));// eslint-disable-line no-console
