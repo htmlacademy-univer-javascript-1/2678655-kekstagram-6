87f9ef13f28b3631dfc2ed1b5aff7dee62613428
Keks <keks@htmlacademy.ru>
2025-11-21 16:07:35 +0300

    :heavy_check_mark: Сборка #4

--- 4/js/functions.js
+++ 4/js/functions.js
@@ -1,14 +1,12 @@
 function isLengthValid(input, len) {
   return input.toString().length <= len;
 }
-console.log(isLengthValid('test', 5));// eslint-disable-line no-console
 
 function isPalindrome(input) {
   const validateStroke = input.toString().replaceAll(' ', '').toLowerCase();
   const reversedStroke = validateStroke.split('').reverse().join('');
   return validateStroke === reversedStroke;
 }
-console.log(isPalindrome('А роза упала на лапу Азора'));// eslint-disable-line no-console
 
 function getDigit(input) {
   if (typeof input === 'number') {
@@ -24,4 +22,3 @@ function getDigit(input) {
   return result ? Number(result) : NaN;
 }
 
-console.log(getDigit('а я томат, железный ламинат-31231'));// eslint-disable-line no-console
