edb73662174afb45e8c9f53139c032acf6be5106
Egor Osipchuk <kaban.kabanytch@yandex.ru>
2025-11-27 20:51:38 +0700

    разбил код на модули

---
+++ js/photos.js
@@ -0,0 +1,18 @@
+import { PHOTO_COUNT, DESCRIPTION } from './data.js';
+import { getCount, getRandomInteger, getRandomArrayElement } from './utils.js';
+import { createComments } from './comments.js';
+
+const createSequentialId = getCount();
+
+const createPhoto = () => {
+  const id = createSequentialId();
+  return {
+    id,
+    url: `photos/${id}.jpg`,
+    description: getRandomArrayElement(DESCRIPTION),
+    likes: getRandomInteger(15, 200),
+    comments: createComments()
+  };
+};
+
+export const similarPhoto = () => Array.from({ length: PHOTO_COUNT }, createPhoto);
