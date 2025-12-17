7de0172ab0abc0631105ad097ccf41087d12be39
Keks <keks@htmlacademy.ru>
2025-12-17 15:19:17 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/main.js
@@ -0,0 +1,8 @@
+import { renderPhotos } from './modules/render-photos.js';
+import { initPreviewModal } from './modules/render-preview-modal.js';
+import { initForm } from './modules/form.js';
+
+
+renderPhotos();
+initPreviewModal();
+initForm();
