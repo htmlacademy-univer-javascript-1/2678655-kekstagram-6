73d397c78d40926134c3855105c85522e5e128b7
Keks <keks@htmlacademy.ru>
2025-12-17 09:33:58 +0300

    :heavy_check_mark: Сборка #11

--- 11/js/main.js
+++ 11/js/main.js
@@ -1,6 +1,6 @@
-import { renderPhotos } from './render-photos.js';
-import { initPreviewModal } from './render-preview-modal.js';
-import { initForm } from './form.js';
+import { renderPhotos } from './modules/render-photos.js';
+import { initPreviewModal } from './modules/render-preview-modal.js';
+import { initForm } from './modules/form.js';
 
 
 renderPhotos();
